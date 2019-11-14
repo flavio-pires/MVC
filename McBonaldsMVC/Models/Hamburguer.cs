34c855b61a40ab3bf0a6ea0370c7bc5650502f72
Flavio Pires <fdnpires@gmail.com>
2019-11-14 11:30:45 -0300

    aula_14/11

--- McBonaldsMVC/Models/Hamburguer.cs
+++ McBonaldsMVC/Models/Hamburguer.cs
@@ -2,6 +2,15 @@ namespace McBonaldsMVC.Models
 {
     public class Hamburguer : Produto
     {
-        
+        public Hamburguer()
+        {
+            
+        }
+
+        public Hamburguer(string nome, double preco)
+        {
+            this.Nome = nome;
+            this.Preco = preco;
+        }
     }
 }
---
+++ McBonaldsMVC/Repositories/HamburguerRepository.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.IO;
+using McBonaldsMVC.Models;
+
+namespace McBonaldsMVC.Repositories
+{
+    public class HamburguerRepository
+    {
+        private const string PATH = "Database/Hamburguer.csv";
+
+        public HamburguerRepository()
+        {
+            if(!File.Exists(PATH))
+            {
+                File.Create(PATH).Close();
+            }
+        }
+
+        public List<Hamburguer> ObterTodos()
+        {
+            List<Hamburguer> hamburgueres = new List<Hamburguer>();
+            string[] linhas = File.ReadAllLines(PATH);
+            foreach (var linha in linhas)
+            {
+                Hamburguer h = new Hamburguer();
+                string[] dados = linha.Split(";");
+                h.Nome = dados[0];
+                h.Preco = double.Parse(dados[1]);
+                hamburgueres.Add(h);
+            }
+            return hamburgueres;
+        }
+    }
+}
